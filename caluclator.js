b61409d7a03b75e3042d94c770373552fae49d8c
Gavin Lavergne <gavinlavergne12@gmail.com>
2025-10-06 10:03:25 -0500

    CSS,Claculator, and HTML made

---
+++ caluclator.js
@@ -0,0 +1,10 @@
+const buttonValues = [
+    "AC", "+/-", "%", "Divisionsymbol",
+    "7", "8", "9", "x"
+    "4", "5", "6", "-",
+    "1", "2", "3", "+",
+    "0", ".", "="
+];
+
+const rightSymbols = ["division", "x", "-", "+", "="];
+const topSymbols = ["AC", "+/-", "%"];
